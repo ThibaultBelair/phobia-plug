e472b055618f9233cbdca835e0f4143bf24ccec0
Thibault <belair.thibault@gmail.com>
2019-08-27 14:29:26 +0200

    fonction get images OK, TODO test on a specific site

--- build/options.bundle.js
+++ build/options.bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e3c0f628e59b238f9906";
+/******/ 	var hotCurrentHash = "4c267f61773fec1d25cb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
